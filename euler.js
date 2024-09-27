593343bd9fe1b2583fb737926f84db0860a51582
AnanthuDBS <20033258@mydbs.ie>
2024-09-27 14:42:06 +0100

    code

--- euler.js
+++ euler.js
@@ -1,16 +1,14 @@
-/*If we list all the natural numbers below 10 that are multiples
-of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
+alert("Loading euler.js");
 
-1. Find the sum of all the multiples of 3 or 5 below 1000.
+let a = 3;
+let b = 5;
+let n = 1000;
+let sum = 0;
 
-2. Create a function to find the sum of all the multiples of a or b below n.
-(a,b,n) are function parameters
+for (let i =1; i<n; i++) {
+    if (i%a ==0 || i%b == 0){
+        sum = sum+i;
+    }
+}
 
-3. Allow the user to enter the parameters via text box(es)
-and alert them to the output
-*/
-alert("Loaded Euler JS");
-a=3;
-b=5;
-n=1000;
+alert("The sum of all multiples i " + sum);
